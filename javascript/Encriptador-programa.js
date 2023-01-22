3e378a20b091be9d38ec53f7fa943ea1817d8002
Carov16 <carolina_velez_vinueza@outlook.com>
2023-01-22 18:08:18 -0500

    agregar funcion boton encriptar

--- javascript/Encriptador-programa.js
+++ javascript/Encriptador-programa.js
@@ -4,3 +4,17 @@ var resultado = document.querySelector(".resultado");
 var mensajeEncriptado = document.querySelector(".mensaje-encriptado")
 
 
+
+function encriptar(mensajeCapturado){
+    let codigoEncritacion = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    mensajeCapturado = mensajeCapturado.toLowerCase();
+
+    for(i=0; i < codigoEncritacion.length; i++){
+        if (mensajeCapturado.includes(codigoEncritacion[i][0])){
+            mensajeCapturado = mensajeCapturado.replaceAll(codigoEncritacion[i][0], codigoEncritacion[i][1])
+
+        }
+    }
+
+    return mensajeCapturado;
+}
